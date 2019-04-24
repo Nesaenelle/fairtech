3ce4f9cb102cced754eabba78b80ec7d5eda99d9
nesaenelle <nesaenelle@gmail.com>
2019-04-24 22:39:45 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
 
+    var toggleLightBtn = $('[data-toggle-light]');
 
+    toggleLightBtn.on('click', function() {
+        $('body').toggleClass('on'); 
+    });
+
+    var $lamp = $('.lamp');
+    
+    function blink() {
+        var random = randomInteger(1, 5);
+        setTimeout(function() {
+            $lamp.addClass('blink');
+           
+            setTimeout(function() {
+                blink();
+
+                $lamp.removeClass('blink');
+            }, 100);
+        }, random * 1000);
+    }
+
+    function randomInteger(min, max) {
+        var rand = min - 0.5 + Math.random() * (max - min + 1)
+        rand = Math.round(rand);
+        return rand;
+      }
+    
+
+    blink();
 
 })
